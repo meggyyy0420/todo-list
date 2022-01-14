6b8459794adc65bd032861220a449ed4ea246b89
amberrr0420 <amberrr0420@gmail.com>
2022-01-14 20:05:14 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -45,6 +45,14 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log('error'))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`APP is running on http:localhost:${port}`)
 })
